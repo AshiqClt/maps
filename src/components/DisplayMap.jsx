46aa02ef9cad8d913c253bacf2097d323e4af21b
Ashiq <ashiq.mayanad@gmail.com>
2023-06-30 07:43:09 +0530

    implemented logics and basic styling

---
+++ src/components/DisplayMap.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const DisplayMap = () => {
+  return (
+    <div style={{ width: "80%" }}>
+      <MapContainer
+        center={selectedLocationCoordinates}
+        key={`${selectedLocationCoordinates[0]}-${selectedLocationCoordinates[1]}`}
+        zoom={12}
+        style={{ height: "97vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {selectedLocation && (
+          <Marker position={selectedLocationCoordinates}>
+            <Popup>{selectedLocation.display_name}</Popup>
+          </Marker>
+        )}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default DisplayMap;
